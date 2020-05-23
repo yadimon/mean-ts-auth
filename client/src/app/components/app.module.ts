ea2754f3781f304ce93d7c75edde5dbbacfe6fa0
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2020-05-24 01:39:31 +0200

    angular 9 update

--- client/src/app/components/app.module.ts
+++ client/src/app/components/app.module.ts
@@ -1,23 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-
 import {AppComponent} from './app.component';
 import {TestComponentComponent} from './test-component/test-component.component';
 import {ApiRequestsService} from '../services/api-requests.service';
-import {HttpModule} from '@angular/http';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {ConfigurationService} from '../services/configuration.service';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import {RouterModule} from "@angular/router";
+import {SignupComponent} from './signup/signup.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {ProfileComponent} from './profile/profile.component';
 import {routing} from "./app.routing";
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {AuthGuard} from "../auth/auth.guard";
 import {AuthInterceptor} from "../auth/auth-interceptor.service";
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -33,7 +32,6 @@ import {AuthInterceptor} from "../auth/auth-interceptor.service";
     BrowserModule,
     FormsModule,
     CommonModule,
-    HttpModule,
     HttpClientModule,
     routing,
   ],
